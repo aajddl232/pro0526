3e75a59e5eda8f28217055556c7a520264d876d8
aajddl232 <rowktlr3368@gmail.com>
2024-11-26 09:26:38 +0900

    Add files via upload

---
+++ js/cstom.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+    });
+
+
+    $('.product_slide').slick({
+        arrows: false,
+        slidesToShow: 3,
+        dots: true, 
+        
+    });
+
+})
